aed66cd1f2ce5e6f1a106c079f2c72bafc280942
sahin Islam <sahinislam878@gmail.com>
2025-08-06 16:56:00 +0530

    Progress: Completed View Data from Udemy

--- src/main/java/com/sahin/JobApp/JobController.java
+++ src/main/java/com/sahin/JobApp/JobController.java
@@ -5,9 +5,12 @@ import com.sahin.JobApp.model.JobPost;
 import com.sahin.JobApp.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class JobController {
@@ -29,5 +32,13 @@ public class JobController {
     public String handleForm(JobPost jobPost) {
         service.addJob(jobPost);
         return "success";
+    }
+
+    @GetMapping("viewalljobs")
+    public String viewAlJobs(Model m){
+        List<JobPost> jobs = service.getAllJobs(); // Fetching Data from services
+        m.addAttribute("jobPosts", jobs); // Sending data To the view
+
+        return "viewalljobs";
     }
 }
